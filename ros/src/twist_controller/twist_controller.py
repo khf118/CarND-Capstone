a2b3d231c30615df5428118545b1bfdfeb079b66
LSH <foxliu2012@gmail.com>
2018-08-11 21:51:24 +0800

    add

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -15,7 +15,7 @@ class Controller(object):
         ki = 0.1
         kd = 0.0
         mn = 0. #minimum throttle value
-        mx = 0.3 #maximum throttle value
+        mx = 0.4 #maximum throttle value
         self.throttle_controller = PID(kp, ki, kd, mn, mx)
 
         tau = 0.5 # 1/(2pi * tau) = cutoff frequency
